8727fe6866dca0e5e0876ab82cfde721f6240444
shirley.pham <thipham1123@gmail.com>
2026-05-25 21:52:16 +0700

    🌻250526: show calendar days

--- src/app/components/calendar/calendar.ts
+++ src/app/components/calendar/calendar.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { ChevronLeft } from "../icons/chevron-left/chevron-left";
-import { ChevronRight } from "../icons/chevron-right/chevron-right";
+import { Component, computed, signal } from '@angular/core';
+import { ChevronLeft } from '../icons/chevron-left/chevron-left';
+import { ChevronRight } from '../icons/chevron-right/chevron-right';
 
 type CalendarDay = {
   date: Date;
@@ -15,4 +15,51 @@ type CalendarDay = {
   templateUrl: './calendar.html',
   styleUrl: './calendar.css',
 })
-export class Calendar {}
+export class Calendar {
+  currentDate = signal(new Date());
+  weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+  monthLabel = computed(() => {
+    return this.currentDate().toLocaleDateString('en-US', {
+      month: 'long',
+      year: 'numeric',
+    });
+  });
+
+  private isSameDate(a: Date, b: Date): boolean {
+    return (
+      a.getFullYear() === b.getFullYear() &&
+      a.getMonth() === b.getMonth() &&
+      a.getDate() === b.getDate()
+    );
+  }
+
+  calendarDays = computed<CalendarDay[]>(() => {
+    const date = this.currentDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    const startDay = firstDayOfMonth.getDay();
+
+    const firstDateInCalendar = new Date(year, month, 1 - startDay);
+
+    const today = new Date();
+
+    const days: CalendarDay[] = [];
+
+    for (let i = 0; i < 42; i++) {
+      const current = new Date(firstDateInCalendar);
+      current.setDate(firstDateInCalendar.getDate() + i);
+
+      days.push({
+        date: current,
+        dayNumber: current.getDate(),
+        isCurrentMonth: current.getMonth() === month,
+        isToday: this.isSameDate(current, today),
+      });
+    }
+
+    return days;
+  });
+}
